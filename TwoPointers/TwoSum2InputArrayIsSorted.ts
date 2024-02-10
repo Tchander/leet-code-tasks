4800141f38b7700a3b1035daba95a12747ff691c
Tchander <evgeniy.tchander@yandex.ru>
2024-02-10 22:33:09 +0300

    Added Two Sum II Input Array is Sorted task

---
+++ TwoPointers/TwoSum2InputArrayIsSorted.ts
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/submissions/1171682898/
+
+const twoSum2InputArrayIsSorted = [2, 7, 11, 15];
+const twoSum2Target = 9;
+
+function twoSum(numbers: number[], target: number): number[] {
+  const numberMap = new Map()
+  for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i]
+    const difference = target - number
+    if (numberMap.has(difference)) return [numberMap.get(difference) + 1, i + 1]
+    numberMap.set(number, i)
+  }
+  return [];
+}
+
+console.log(twoSum(twoSum2InputArrayIsSorted, twoSum2Target)); // [1, 2]
--- TwoPointers/ValidPalindrome.ts
+++ TwoPointers/ValidPalindrome.ts
@@ -13,3 +13,22 @@ function isPalindrome(s: string): boolean {
 console.log(isPalindrome(validPalindrome1)); // true
 console.log(isPalindrome(validPalindrome2)); // false
 console.log(isPalindrome(validPalindrome3)); // true
+
+/* Best Solution */
+// function isPalindrome(s: string): boolean {
+//   const array = s
+//     .toLowerCase()
+//     .replace(/[^A-Za-z0-9]/g, '')
+//     .replace(/\s/g, '')
+//     .split('');
+//
+//   for (let i = 0; i < array.length; i++) {
+//     const first = array[i];
+//     const second = array[array.length - 1 - i];
+//
+//     if (first !== second) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
